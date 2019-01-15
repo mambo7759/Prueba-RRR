c5fca3cbad0037911d9b1efe6f2933ace49351fe
mambo7759 <46626494+mambo7759@users.noreply.github.com>
2019-01-14 21:19:24 -0500

    Add files via upload

---
+++ Ensayo.R
@@ -0,0 +1,11 @@
+hello <- "Hello"
+world <- "world"
+date <- (Sys.time())
+
+
+text<-"For this time, i gonna show you the best way to improve the taking and correct decisions for the unbelievable profits"
+print(paste( hello, world, date,text,Sep=""))
+plot (23)
+plot (12:23)
+plot(tabla.pesos)
+mean (tabla.pesos)
